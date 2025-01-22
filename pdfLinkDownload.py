dfb84b385867f31d030c41d9c633fdbac7c6c7a5
JOSE LUNA <jose.luna@icarus.cl>
2025-01-22 12:40:07 -0300

    Permite obtener token para descargas

--- pdfLinkDownload.py
+++ pdfLinkDownload.py
@@ -5,6 +5,8 @@ import requests
 import pdfplumber
 import time
 import shutil
+import json
+
 # import urllib.parse
 from urllib.parse import urlparse, unquote
 
@@ -214,23 +216,24 @@ def process_pdfs_in_folder(folder_path, orderBy="equipo"):
                                f"Tiempo en procesar este pdf {formato_hms} segundos"]
                 log_mesajes(log_entries, conf.ruta_global, "log.txt", True)
 
-
-def download_oferts_pdf(links, output_folder):
+def download_files(links, token, output_folder,fileName,fileExtension):
     downloaded_files = []
-    token = conf.token
     tenat = conf.tenat
-    headers = conf.headers
+    headers = {
+        "User-Agent": "JFL/0.0.1",
+        "X-Tenant": tenat,
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
     for link in links:
         # Decodifica la URL y extrae el nombre del archivo
         decoded_link = unquote(link)
-        # sanitized_link = urllib.parse.quote(link, safe=':/') # Esta opcion sanitiza los acentos y me falla por que algunas url tiene acentos.
-        sanitized_link = link.replace("+", "%2B")
-        # Extrae el nombre del archivo desde la URL y lo sanitiza
         ofertNumber = sanitize_filename(os.path.basename(urlparse(decoded_link).path))
         response = requests.get(link, headers=headers)
         if response.status_code == 200:
             content_disposition = response.headers.get('Content-Disposition')
-            filename = f"Oferta{ofertNumber}.pdf"
+            filename = f"{fileName}{ofertNumber}.{fileExtension}"
             file_path = os.path.join(output_folder, filename)
             with open(file_path, 'wb') as f:
                 f.write(response.content)
@@ -243,6 +246,17 @@ def download_oferts_pdf(links, output_folder):
                         conf.ruta_global, "log.txt", True)
     return downloaded_files
 
+def get_login_token():
+    response = requests.post(conf.logingUrl, headers=conf.headers, data=json.dumps(conf.data))
+    if response.status_code == 200:
+        response_json = response.json()
+        token = response_json.get("plain-text-token")
+        if token:
+            return {"token":token}
+        else:
+            return {"error":"El token no se encontró en la respuesta."}
+    else:
+        return {"error":f"Error al intentar login Status Code: {response.status_code}"}
 
 inicio = time.time()
 folder_path = conf.ruta_global
@@ -255,22 +269,38 @@ log_mesajes(mensajes, folder_path, "log.txt", True)
 log_mesajes(f"Procesando carpeta: {folder_path}", folder_path, "log.txt", True)
 log_mesajes(f"Parte el análisis de los documentos", folder_path, "log.txt", True)
 # Proceso para obtener los pdf
-oferts = [146, 339, 6, 354, 101, 88, 355, 102, 356, 103, 148, 357, 149, 262, 358, 119, 105, 426, 93, 145, 243, 272, 330,
-          359, 106, 143, 360, 107, 141, 274, 341, 361, 186, 75, 189, 244, 245, 276, 338, 345, 348, 206, 246, 255, 268,
-          283, 349, 370, 384, 207, 247, 273, 284, 296, 373, 391, 430, 231, 285, 297, 377, 227, 298, 378, 397, 299, 232,
-          275, 286, 233, 287, 311, 444, 251, 295, 300, 380, 220, 248, 277, 288, 308, 374, 433, 237, 313, 381, 301, 382,
-          411, 302, 383, 416, 289, 312, 228, 304, 386, 229, 303, 385, 418, 305, 421, 290, 291, 307, 458, 317, 342, 394,
-          187, 362, 120, 121, 122, 123, 109, 363, 124, 260, 201, 364, 392, 461, 170, 365, 111, 367, 112, 113, 368, 114,
-          369, 181, 372, 292, 242, 293, 306, 375, 460, 125, 387, 462, 115, 150, 379, 116, 151, 388, 117, 325, 326, 309,
-          327, 409, 279, 328, 413, 486, 329, 414, 344, 415, 89, 393, 90, 403, 171, 183, 154, 408, 155, 417, 128, 464,
-          218, 465, 129, 184, 118, 131, 132, 133, 134, 135, 136, 91, 137, 95, 96, 259, 98, 99, 94, 353, 352, 366, 351,
-          202, 182, 280, 337, 459, 447, 376, 264]
+oferts = []
+#oferts = [146, 339, 6, 354, 101, 88, 355, 102, 356, 103, 148, 357, 149, 262, 358, 119, 105, 426, 93, 145, 243, 272, 330,
+#           359, 106, 143, 360, 107, 141, 274, 341, 361, 186, 75, 189, 244, 245, 276, 338, 345, 348, 206, 246, 255, 268,
+#           283, 349, 370, 384, 207, 247, 273, 284, 296, 373, 391, 430, 231, 285, 297, 377, 227, 298, 378, 397, 299, 232,
+#           275, 286, 233, 287, 311, 444, 251, 295, 300, 380, 220, 248, 277, 288, 308, 374, 433, 237, 313, 381, 301, 382,
+#           411, 302, 383, 416, 289, 312, 228, 304, 386, 229, 303, 385, 418, 305, 421, 290, 291, 307, 458, 317, 342, 394,
+#           187, 362, 120, 121, 122, 123, 109, 363, 124, 260, 201, 364, 392, 461, 170, 365, 111, 367, 112, 113, 368, 114,
+#           369, 181, 372, 292, 242, 293, 306, 375, 460, 125, 387, 462, 115, 150, 379, 116, 151, 388, 117, 325, 326, 309,
+#           327, 409, 279, 328, 413, 486, 329, 414, 344, 415, 89, 393, 90, 403, 171, 183, 154, 408, 155, 417, 128, 464,
+#           218, 465, 129, 184, 118, 131, 132, 133, 134, 135, 136, 91, 137, 95, 96, 259, 98, 99, 94, 353, 352, 366, 351,
+#           202, 182, 280, 337, 459, 447, 376, 264]
 #oferts = [6,75]
-ofertsLinks = [conf.getOfertsUrl + str(ofert) for ofert in oferts]
-download_oferts_pdf (ofertsLinks,folder_path) # para que esto no de error hay que asegurar obtener el token de un usario logeado
+
+
+if len(oferts):
+    ofertsLinks = [conf.getOfertUrl + str(ofert) for ofert in oferts]
+    excelLinks = [conf.getExcelUrl + str(ofert) for ofert in oferts]
+    token = get_login_token()
+    if token.get('token') :
+        log_mesajes("loging exitoso", conf.ruta_global, "log.txt", True)
+        ofertFiles = download_files(ofertsLinks, token.get('token'), folder_path, "Oferta", "pdf")
+        excelFiles = download_files(excelLinks, token.get('token'), folder_path, "Eett", "xlsx")
+        log_mesajes(f"PDF obtenidos/ofertas: {len(ofertFiles)}/{len(oferts)}", conf.ruta_global, "log.txt", True)
+        log_mesajes(f"XLSX obtenidos/ofertas: {len(excelFiles)}/{len(oferts)}", conf.ruta_global, "log.txt", True)
+    else:
+        log_mesajes(token.get('error'), conf.ruta_global, "log.txt", True)
+        log_mesajes(token.get('error'), conf.ruta_global, "ERROR.log", False)
 
 # Procesa de los pdf descargados anterior mente o que se encuentren en la carpta
 process_pdfs_in_folder(folder_path, "equipo")
+# process_pdfs_in_folder(folder_path, "sinCarpetas")
+# process_pdfs_in_folder(folder_path, "proponente")
 fin = time.time()
 log_mesajes(f"Termina el análisis de los documentos:", folder_path, "log.txt", True)
 formato_hms = elapsed_time(fin - inicio)
